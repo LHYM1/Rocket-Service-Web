ecd649120dac73c3a8c616185124e1804977064c
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2025-12-30 16:38:33 -0500

    Campos formulario login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -5,28 +5,58 @@ function iniciarsesion() {
         usuario: "",
         contraseña: ""
     });
+
+
+    const handleChange = (e) => {
+        setForm ({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // simulación envío de datos
+        console.log("Datos del formulario:", form);
+
+        if (form.usuario === "" || form.contrasena === "") {
+        alert("Todos los campos son obligatorios");
+        return;
+        }
+
+        // Aquí va el código de autenticación
+        alert("Inicio de sesión exitoso");
+
+    };
+
+    return (
+        <div>
+            <h2>Inicio de Sesión</h2>
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                <label>Usuario</label>
+                <input
+                    type="text"
+                    name="usuario"
+                    value={form.usuario}
+                    onChange={handleChange}
+                />
+                </div>
+
+                <div>
+                <label>Contraseña</label>
+                <input
+                    type="password"
+                    name="contrasena"
+                    value={form.contrasena}
+                    onChange={handleChange}
+                />
+                </div>
+            </form>
+        </div>
+    );
 }
 
-const handleChange = (e) => {
-    setForm ({
-        ...form,
-        [e.target.name]: e.target.value
-    });
-};
-
-const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // simulación envío de datos
-    console.log("Datos del formulario:", form);
-
-    if (form.usuario === "" || form.contrasena === "") {
-      alert("Todos los campos son obligatorios");
-      return;
-    }
-
-    // Aquí va el código de autenticación
-    alert("Inicio de sesión exitoso");
-
-};
 export default login;
