cf8c89b428b895a3be582b009ab497c0e98c9e5e
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2025-12-30 16:21:46 -0500

    login.jsx función validación usuario y contraseña

--- src/login.jsx
+++ src/login.jsx
@@ -17,9 +17,14 @@ const handleChange = (e) => {
 const handleSubmit = (e) => {
     e.preventDefault();
 
-    // simulación envio de datos
-    console.log("Dattos del formulario:", form);
+    // simulación envío de datos
+    console.log("Datos del formulario:", form);
 
+    if (form.usuario === "" || form.contrasena === "") {
+      alert("Todos los campos son obligatorios");
+      return;
+    }
+    
 
 }
 export default login;
