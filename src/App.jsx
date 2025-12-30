cc978013216a58ddd2e72a0732a5e1d0eaeee229
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2025-12-30 12:41:44 -0500

    Código container botón

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
+    <div className="container">
+      <button className='neon-3d-button'>Inicio sesión</button>
+    </div>
   )
 }
 
