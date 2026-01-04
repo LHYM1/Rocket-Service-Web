d3b64a6babf125e47dbe20d7bc67cc52864cc82e
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-04 11:04:18 -0500

    Quitando código App.jsx para colocarlo en login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,1 @@
 
-import './App.css'
-
-function App() {
-  return (
-    <div className="container">
-      <button className='neon-3d-button'>Inicio sesión</button>
-    </div>
-  )
-}
-
-export default App
