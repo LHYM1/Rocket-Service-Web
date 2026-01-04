8b12ffd2416dcb5cde15759a2c47b749f1139439
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-04 12:40:43 -0500

    Código creación de rutas App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } 
+from 'react-router-dom';
+import login from "./login";
+import react from 'react';
 
+function App() {
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<login />} />
+      </Routes>
+    </Router>  
+    </>
+  )
+}
+
+export default App
