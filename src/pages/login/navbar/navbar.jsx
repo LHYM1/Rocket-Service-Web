425fd03ccdddf9b5f197e38b9340bf1469c3e2e0
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-05 18:25:28 -0500

    Código navbar.jsx

---
+++ src/pages/login/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <section className="header">
+            <h1 className="empresa">Rocket Service</h1>
+            <nav className="navbar">
+                <ul className="navLinks">
+                    <li>
+                        <link to = "/" >Home</link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="icons">
+                <button className="search-button">
+                    <i className="fas fa-search"></i>
+                </button>
+
+                <link to = "/Notifications" className="nitification-icon">
+                    <i className="fa-regular fa-bell"></i>
+                    <span className="counter">0</span>
+                </link>
+            </div>
+        </section>
+    )
+}
+
+export default Navbar;
