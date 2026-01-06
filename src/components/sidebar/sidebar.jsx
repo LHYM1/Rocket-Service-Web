e83303b5da7b5f1c168e01c9f09510ab9504c99b
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-06 15:38:49 -0500

    Código Sidebar.jsx

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -14,6 +14,7 @@ const Sidebar = () => {
 
         <div className="menu-list">
             <a href="#" className="item"></a>
+            <i class="fa-solid fa-users"></i>
             Usuarios
         </div>
         
