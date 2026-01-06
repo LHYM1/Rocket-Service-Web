6afb5e04aa1fc3a5925236d2f7e1ab8bd9077bba
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-06 16:07:03 -0500

    Definición y creación de rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } 
 from 'react-router-dom';
 import Login from "./pages/login/login.jsx";
+import DashboardLayout from './layouts/DashboardLayout';
+import Dashboard from './pages/dashboard/Dashboard';
+import Profile from './pages/profile/Profile';
 
 function App() {
 
   return (
-    <>
     <Router>
       <Routes>
+
         <Route path="/" element={<Login />} />
+
+        <Route path="dashboard" element={<DashboardLayout />}>
+           <Route path="dashboard" element={<Dashboard />} />  
+           <Route path="profile" element={<Profile />} />       
+        </Route>
+
       </Routes>
     </Router>  
-    </>
-  )
+  
+  );
 }
 
 export default App;
