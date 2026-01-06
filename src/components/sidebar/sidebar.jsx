7d67b7c2f83aa36279d92527c952b71ada1561a9
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-06 15:35:53 -0500

    Código Sidebar.jsx

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return <div className="menu">
+        <div className="name-company">
+            <h2>Rocket Service</h2>
+        </div>
+
+        <div className="menu-list">
+            <a href="#" className="item"></a>
+            <i class="fa-solid fa-house"></i>
+            Dasdboard
+        </div>
+
+        <div className="menu-list">
+            <a href="#" className="item"></a>
+            Usuarios
+        </div>
+        
+        <div className="menu-list">
+            <a href="#" className="item"></a>
+            Insumos
+        </div>
+    </div>;
+};
+
+export default Sidebar;
