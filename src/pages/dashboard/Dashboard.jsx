8f720fcf6774a378507d5ea75b9e4600d4ccee37
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-12 17:34:08 -0500

    Creando cards Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,7 +4,20 @@ import './Dashboard.css';
 const Dashboard = () => {
     return (
         <div className="dashboard-page"> 
-            <h2>Bienvenido al Dashboard</h2>
+            <div className="dashb-ordTot">
+                <h2>Ordenes totales</h2>
+                <span className="counter">0</span>
+            </div>
+
+            <div className="dashb-ordPend"> 
+                <h2>Ordenes Pendientes</h2>
+                <span className="counter">0</span>
+            </div>
+
+            <div className="dashb-ordFinl"> 
+                <h2>Ordenes Finalizadas</h2>
+                <span className="counter">0</span>
+            </div>
         </div>
     );
 };
