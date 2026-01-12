0e106a3162622b3e9dce581b48607fe7d0e4ae79
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-12 17:42:21 -0500

    Dashboard.jsx cambiando className a cards

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,17 +4,17 @@ import './Dashboard.css';
 const Dashboard = () => {
     return (
         <div className="dashboard-page"> 
-            <div className="dashb-ordTot">
+            <div className="card-ordTot">
                 <h2>Ordenes totales</h2>
                 <span className="counter-dash">0</span>
             </div>
 
-            <div className="dashb-ordPend"> 
+            <div className="card-ordPend"> 
                 <h2>Ordenes Pendientes</h2>
                 <span className="counter-dash">0</span>
             </div>
 
-            <div className="dashb-ordFinl"> 
+            <div className="card-ordFinl"> 
                 <h2>Ordenes Finalizadas</h2>
                 <span className="counter-dash">0</span>
             </div>
