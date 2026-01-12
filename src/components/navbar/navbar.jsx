3576cebd6b205395803068888110021e536e21fe
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-12 14:35:21 -0500

    Cambios Navbar.css y navbar.jsx estilos name empresa

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <section className="header">
             <h1>
                 <span className="empresa-name1">Rocket</span>
-                <span className="empesa-name2">Service</span>
+                <span className="empresa-name2"> Service</span>
             </h1>
             <nav className="navbar">
                 <ul className="navLinks">
