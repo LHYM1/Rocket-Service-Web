a7eb0bc07f14c4d64cf65566effe9393d27dd24e
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-18 12:14:22 -0500

    Código cards Dashboard.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,23 +1,24 @@
 import 'react-router-dom';
+import ChardCard from '../../components/chartCard/ChartCard';
+import TableCard from '../../components/tableCard/tableCard';
+import Card from '../../components/card/Card';
 import './Dashboard.css';
 
 const Dashboard = () => {
     return (
-        <div className="dashboard-page"> 
-            <div className="card-ordTot">
-                <h2>Ordenes totales</h2>
-                <span className="counter-dash">0</span>
-            </div>
+        <div className="dashboard-page">
 
-            <div className="card-ordPend"> 
-                <h2>Ordenes Pendientes</h2>
-                <span className="counter-dash">0</span>
-            </div>
+            <Card title="Órdenes totales">
+                <h2>0</h2>
+            </Card>
 
-            <div className="card-ordFinl"> 
-                <h2>Ordenes Finalizadas</h2>
-                <span className="counter-dash">0</span>
-            </div>
+            <Card title="Ordenes pendientes"> 
+                <h2>0</h2>
+            </Card>
+
+            <Card title="Ordenes finalizadas"> 
+                <h2>0</h2>
+            </Card>
         </div>
     );
 };
