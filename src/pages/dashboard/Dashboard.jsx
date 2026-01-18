91104623713c778f84f0d7a7cfb1bbae0dc80935
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-18 13:20:37 -0500

    Envolviendo cards dentro de dash-container

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
-import 'react-router-dom';
-import ChardCard from '../../components/chartCard/ChartCard';
+
+import ChartCard from '../../components/chartCard/ChartCard';
 import TableCard from '../../components/tableCard/tableCard';
 import Card from '../../components/card/Card';
 import './Dashboard.css';
@@ -8,19 +8,21 @@ const Dashboard = () => {
     return (
         <div className="dashboard-page">
             <div className="dash-container">
+
                 <Card title="Órdenes totales">
                     <h2>0</h2>
                 </Card>
+
+                <Card title="Ordenes pendientes"> 
+                    <h2>0</h2>
+                </Card>
+
+                <Card title="Ordenes finalizadas"> 
+                    <h2>0</h2>
+                </Card>
+
             </div>
-            
-
-            <Card title="Ordenes pendientes"> 
-                <h2>0</h2>
-            </Card>
-
-            <Card title="Ordenes finalizadas"> 
-                <h2>0</h2>
-            </Card>
+        
         </div>
     );
 };
