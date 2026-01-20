2b2e890b24643771c566c9ea2db3d7d5cea4029e
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-20 08:35:06 -0500

    Correción código Card.jsx, App.jsx e index.css para evitar conflictos con cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Login from "./pages/login/login.jsx";
 import DashboardLayout from './layouts/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
 
+
 import Content from './components/content/Content';
 import Profile from './components/profile/Profile';
+import Card from './components/card/Card';
 
 function App() {
 
@@ -18,7 +20,8 @@ function App() {
         <Route path="/dashboard" element={<DashboardLayout />}>
            <Route index element={<Dashboard />} />  
            <Route path="content" element={<Content />} />
-           <Route path="profile" element={<Profile />} />    
+           <Route path="profile" element={<Profile />} />  
+           <Route path="card" element={<Card />} />  
         </Route>
 
       </Routes>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,10 @@
-import './Card.css';
+import styles from'./Card.module.css';
 
 function Card({ title, children, className = '' }) {
     return (
-        <div className={`card ${className}`}>
-            { title && <h3 className="card-title">{title}</h3> }
-            <div className="card-body">
+        <div className={`${styles.card} ${className}`}>
+            { title && <h3 className={styles.title}>{title}</h3> }
+            <div className={styles.body}>
                 {children}
             </div>
         </div>
