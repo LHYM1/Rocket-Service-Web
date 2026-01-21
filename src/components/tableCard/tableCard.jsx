6e2ba5598e5dd008972b600a28739c1701430088
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-20 22:23:15 -0500

    Código TableCard.jsx

--- src/components/tableCard/tableCard.jsx
+++ src/components/tableCard/tableCard.jsx
@@ -1,9 +1,30 @@
 import Card from '../card/Card';
+import styles from './TableCard.module.css';
 
-function TableCard({ title, children }) {
+function TableCard({ title, columns, data }) {
     return (
-        <Card title={title} className="table-card">
-            {children}
+        <Card title={title}>
+            <div className={styles.tableWrapper}>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            {columns.map((col, index) => (
+                                <th key={index}>{col}</th>
+                            ))}
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {data.map((row, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {columns.map((col, colIndex) => (
+                                    <td key={colIndex}>{row[col]}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </Card>
     );
 }
