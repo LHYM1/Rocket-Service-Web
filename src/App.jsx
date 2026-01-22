fa56cf0e28206d261f698fc264b31091d851d1ea
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-22 11:46:12 -0500

    Importación ruta Report.jsx en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ from 'react-router-dom';
 import Login from "./pages/login/login.jsx";
 import DashboardLayout from './layouts/DashboardLayout';
 import Dashboard from './pages/dashboard/Dashboard';
+import Report from './pages/reports/Report';
 
 
 import Content from './components/content/Content';
 import Profile from './components/profile/Profile';
-import Card from './components/card/Card';
 
 function App() {
 
@@ -18,12 +18,12 @@ function App() {
         <Route path="/" element={<Login />} />
 
         <Route path="/dashboard" element={<DashboardLayout />}>
-           <Route index element={<Dashboard />} />  
-           <Route path="content" element={<Content />} />
-           <Route path="profile" element={<Profile />} />  
-           <Route path="card" element={<Card />} />  
+          <Route index element={<Dashboard />} /> 
+          <Route path="content" element={<Content />} />
+          <Route path="profile" element={<Profile />} /> 
+          <Route path="/reports" element={<Report />} />   
         </Route>
-
+           
       </Routes>
     </Router>  
   
