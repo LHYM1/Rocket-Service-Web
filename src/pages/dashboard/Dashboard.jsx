5c1b1c42b34a79725915d5ecbd201c3ac02e63c1
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-22 11:10:01 -0500

    Quitando código tabla de Dashboard.jsx para usarlo en Report.jsx

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,12 +5,6 @@ import Card from '../../components/card/Card';
 import './Dashboard.css';
 
 const Dashboard = () => {
-
-    const orders = [
-        { id: 1, client: 'Juan', status: 'Pendiente', total: 120000 },
-        { id: 2, client: 'Ana', status: 'Finalizado', total: 85000 },   
-    ];
-
     return (
         <div className="dashboard-page">
             <div className="dash-container">
@@ -53,11 +47,6 @@ const Dashboard = () => {
                 </Card>
 
             </div>
-
-            <TableCard 
-                title="Ordenes recientes"
-                data={orders}
-            />
         
         </div>
     );
