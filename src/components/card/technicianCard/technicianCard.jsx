8dcf49dc11537a5e5945d29dd6b2aa103cb29f82
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-25 12:08:57 -0500

    Estilos para cards técnicos

--- src/components/card/technicianCard/technicianCard.jsx
+++ src/components/card/technicianCard/technicianCard.jsx
@@ -3,29 +3,38 @@ import Card from '../Card';
 
 const TechnicianCard = ({ tecnico, asignar }) => {
     return (
-        <Card title={tecnico.nombre}>
+        <Card>
+            <div className="technician-content">
 
-            <p className="p-text">
-                <strong>Estado:</strong>{" "}
-                <span
-                    className={`estado ${
-                    tecnico.estado === "Disponible" /*Operador ternario (indicar estado)*/
-                        ? "disponible"
-                        : "ocupado"
-                    }`}
-                >
-                    {tecnico.estado}
+                <div className="technician-info">
+                    <h4>{tecnico.nombre}</h4>
 
-                </span>
-            </p>
-            
-            <button
-                className="btn-asignar"
-                disabled={tecnico.estado !== "Disponible"}
-                onClick={() => asignar(tecnico.id)}  
-            >
-                Asignar orden
-            </button>
+                    <p className="p-text">
+                        <strong>Estado:</strong>{" "}
+                        <span
+                            className={`estado ${
+                            tecnico.estado === "Disponible" /*Operador ternario (indicar estado)*/
+                                ? "disponible"
+                                : "ocupado"
+                            }`}
+                        >
+                            {tecnico.estado}
+
+                        </span>
+                    </p>
+                </div>
+                
+                <div className="technician-action">
+                    <button
+                        className="btn-asignar"
+                        disabled={tecnico.estado !== "Disponible"}
+                        onClick={() => asignar(tecnico.id)}  
+                    >
+                        Asignar orden
+                    </button>
+                </div>
+        
+            </div>
 
         </Card>
     );
