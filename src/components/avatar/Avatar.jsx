0613336ce4bf44bacb99da6b56c32c09f9a4d3dc
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-27 08:45:13 -0500

    Código Avatar.jsx utilización de modules

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,32 @@
+import styles from './Avatar.module.css';
+
+const Avatar = ({ name, src, size="md", status }) => {
+
+    const getInitials = (name) => {
+        if (!name) return ""; /* Evita errores si no llega el nombre */
+
+        const words = name.trim().split(" "); /* Separa el nombre */
+
+        const firtsInitial = words[0]?.charAt(0) || ""; /* Primera letra del primer nombre */ 
+        const lastInitial = words.length > 1
+            ? words[words.length - 1].charAt(0) /* Primera letra del último nombre */
+            : "";
+
+        return (firtsInitial + lastInitial).toUpperCase();
+    };
+ 
+    /* Función para colocar img o avatar */
+    const initials = getInitials(name);
+
+    return (
+        <div className={`styles.avatar ${styles[size]} ${styles[status]}`}>
+            {src ? (
+                <img src={src} alt={name} />
+            ) : (
+                <span>{initials}</span>
+            )}
+        </div>
+    );
+}
+
+export default Avatar;
