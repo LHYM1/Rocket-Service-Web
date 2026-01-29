561c1f1dab9b72d785dbb63cec1b928a2fa473c6
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-01-28 21:17:37 -0500

    Código OrderCard.jsx

---
+++ src/components/orders/OrderCard.jsx
@@ -0,0 +1,27 @@
+import styles from './OrderCard.module.css';
+
+const OrderCard = ({ order, activa, onSelect }) => {
+    return (
+        <div 
+            className={`${styles.card} ${activa ? styles.activa : ""}`}
+            onClick={onSelect} //Si el user hace click ejecuta función onSelect
+        >
+
+            <div className={styles.header}>
+                <h4>{order.codigo}</h4>
+                <span className={styles.estado}>{order.estado}</span>
+            </div>
+
+            <p><b>Cliente:</b> {order.cliente}</p>
+            <p><b>Servicio:</b> {order.servicio}</p>
+
+            {/* Letra pequeña para fecha de inicio orden */}
+            <small className={styles.fecha}>
+                Fecha inicio: {order.fechaInicio}
+            </small>
+
+        </div> 
+    );
+}; 
+
+export default OrderCard;
