7b2678baa1a6ff7f5d308bb63d5338e0488c3053
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-19 17:45:21 -0500

    Configuración conexión servidor con db

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log('Base de datos conectada correctamente');
+});
