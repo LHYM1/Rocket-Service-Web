136ff4c9d1e3241f058ae4bbfe7b2b2e7166e1e6
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-19 20:52:18 -0500

    Arreglando acceso a rutas en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors'); // permite acceso a la API desde el frontend
-const usuariosRoutes = require('./routes/usuariosRoutes');
+const userRoutes = require('../users/usuarios.routes');
 
 const app = express();
 
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(cors()); // permite acceso a la API desde el frontend
 
 // Rutas usuario
-app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/usuarios', userRoutes);
 
 // Rutas para clasificación de usuarios
-app.use('/api/clasificacion_de_usuarios', usuariosRoutes);
+app.use('/api/clasificacion_de_usuarios', userRoutes);
 
 module.exports = app;
