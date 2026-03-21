764d927573c5c55c1fd82ae2145a9332a89f5e3e
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-21 16:33:50 -0500

    Código crud frontend para UsersPage

---
+++ frontend/src/pages/usersPage/CardContainer/UsersPage.jsx
@@ -0,0 +1,30 @@
+
+function UsersPage() {
+  const [usuarios, setUsuarios ] = useState([]);
+  const [showModal, setShowModal] = useState(false); // Estado para abrir/cerrar el modal
+  const [eliminar, setEliminar] = useState(false);
+  
+  const getUsuarios = () => {
+    axios.get("http://localhost:4000/api/usuarios/listar")
+    .then((response) => {
+      setUsuarios(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => { // function useEffect para imprimir datos
+    getUsuarios();
+  }, []);
+
+  return (
+    <>
+      <UserForm />
+      <UsersTable usuarios={usuarios} />
+    </>
+  );
+  
+}
+
+export default UsersPage();
