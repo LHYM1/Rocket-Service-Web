382b2189a6c887a4d3a20b5afad8401b5cebafa6
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-22 16:15:25 -0500

    Acceso a rutas (auth.routes)

---
+++ backend/src/modules/auth/auth.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { login } = require("./auth.controller")
+const { verifyRole } = require("../../middlewares/authMiddleware");
+
+const router = express.Router();
+
+// Ruta login
+router.post("/login", login);
+
+// Ruta protegida solo para admin
+router.get("/admin-data", verifyRole(["admin"]), (req, res) => {
+    res.json({ 
+        message: "Bienvenido administrador", 
+        user: req.user 
+    });
+})
+
+// Ruta protegida para técnico
+router.get("/tec-data", verifyRole(["user", "admin"]), (req, res) => {
+    res.json({ 
+        message: "Bienvenido Técnico", 
+        user: req.user 
+    });
+});
