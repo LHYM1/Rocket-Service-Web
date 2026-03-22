d5b77e8cea17a315f3ac823bf18fcaaefea41120
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-22 16:22:46 -0500

    Código auth.controller

---
+++ backend/src/modules/auth/auth.controller.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../../config/db"); // conexion a mysql
+
+const login = async (req, res) => {
+  const { usuario, contrasena } = req.body;
+
+  try {
+    const [rows] = await pool.query("SELECT * FROM users WHERE usuario = ?", [usuario]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    const user = rows[0];
+
+    // Bcrypt para comparar contraseñas
+    if (contrasena !== user.contrasena) {
+      return res.status(401).json({ message: "Contraseña incorrecta" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1min" }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Error en el servidor", error });
+  }
+};
+
+module.exports = { login };
