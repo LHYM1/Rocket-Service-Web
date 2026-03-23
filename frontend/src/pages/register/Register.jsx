3ec22a384925dd44ead3dd4832b92689712824c5
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-23 07:22:17 -0500

    Corrección código Register

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -32,7 +32,7 @@ function Registro() {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/register", {
+      const response = await fetch("http://localhost:4000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
