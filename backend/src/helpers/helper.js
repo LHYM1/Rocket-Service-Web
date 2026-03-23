bb40dc02870a670e3b075a15dc89952417c62954
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-23 16:58:47 -0500

    Código validación de roles

---
+++ backend/src/helpers/helper.js
@@ -0,0 +1,22 @@
+// Protección de rutas
+import { Navigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  const user = jwtDecode(token);
+
+  // Validación del rol
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
