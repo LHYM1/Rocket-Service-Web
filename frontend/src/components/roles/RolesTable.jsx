1fdad5a0494da44551fe1a0a3f4693cb83904845
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-24 19:34:05 -0500

    Código roles table

---
+++ frontend/src/components/roles/RolesTable.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios'
+
+function UsersTable ({ roles, setIdSeleccionado, getRoles}) {
+
+    const eliminarUsuario = (id) => {
+        axios.delete(`http://localhost:4000/api/usuarios/eliminar/${id}`)
+    
+        .then(() => {
+            alert("Usuario eliminado con éxito");
+            getRoles();
+        })
+        .catch(err =>  {
+            console.error(err);
+            alert("No se pudo eliminar el usuario");
+        }); 
+    }
+
+    return (
+        <table className="table table-hover" border="1">
+            <thead className="table-light">
+                <tr>
+                    <th>Id</th>
+                    <th>Categoria</th>
+                    
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {user.map((r) => (
+                    <tr key={r.id_usuario}>
+                        <td>{r.id_usuario}</td>
+                        <td>{r.categoria_usuario}</td>
+                       
+
+                        <td>
+                            <button onClick={() => setIdSeleccionado(u)}
+                                type="button" className="btn btn-warning btn-color">
+                                Editar
+                            </button>
+
+                            <button onClick={() => eliminarUsuario(u.id_usuario)}
+                                type="button" className="btn btn-danger btn-color">
+                                Eliminar
+                            </button>
+                        </td>
+                    </tr>                    
+                ))}
+                
+            </tbody>
+        </table>
+
+    )
+}
+
+export default UsersTable;
+  
