a4719da411df3528e2cb7d11335db9b8624b229c
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-25 07:12:21 -0500

    Ruta orders código

---
+++ backend/src/modules/orders/orders.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controllerOrders = require('./orders.controller');
+
+// Rutas para usuarios
+router.get('/listar', controllerOrders.listarOrdenes);
+router.get('/consultar/:id', controllerOrders.obtenerOrden);
+router.post('/crear', controllerOrders.crearOrden);
+router.put('/modificar/:id', controllerOrders.actualizarOrden);    
+router.delete('/eliminar/:id', controllerOrders.eliminarOrden);
+
+module.exports = router;
