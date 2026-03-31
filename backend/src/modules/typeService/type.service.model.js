cee46c9c0ed8e8c54c354980bce1dc89b5ee3bc5
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-03-31 14:10:40 -0500

    Crud para type.service.model

---
+++ backend/src/modules/typeService/type.service.model.js
@@ -0,0 +1,69 @@
+const db = require ('../../config/db');
+
+const typeService = {
+    finAll: async () => { // Listar
+        const [rows] = await db.query('SELECT * FROM tipo_servicio');
+        return rows;
+    },
+
+    // consultar por id
+    finAll : async (id) => {
+        const [rows] = await db.query(`SELECT * FROM tipo_servicio
+        WHERE id_tipo_servicio = ?`, [id]);
+        return rows[0];
+    },
+
+    // crear
+    create: async (data) => {
+        const {
+            codigo_tipo_servicio,
+            nombre_servicio,
+            descripcion_servicio,
+            costo_servicio
+        } = data;
+
+        const [result] = await db.query(
+            `INSERT INTO tipo_servicio (codigo_tipo_servicio, nombre_servicio,
+            descripcion_servicio, costo_servicio) VALUES (?, ?, ?, ?)`,
+
+            [codigo_tipo_servicio, nombre_servicio, descripcion_servicio, costo_servicio]
+        );
+        return result.insertId;
+    },
+
+    // actualizar
+    update : async (id, data) => {
+        const {
+            codigo_tipo_servicio,
+            nombre_servicio,
+            descripcion_servicio,
+            costo_servicio
+        } = data;
+
+        const [result] = await db.query (
+            `UPDATE tipo_servicio SET codigo_tipo_servicio = ?,
+            nombre_servicio = ?, descripcion_servicio = ?,
+            costo_servicio = ? WHERE id_tipo_servicio = ?`,
+
+            [
+                codigo_tipo_servicio,
+                nombre_servicio,
+                descripcion_servicio,
+                costo_servicio,
+                id
+            ]
+        );
+        return result.affectedRows > 0;
+    },
+
+    // eliminar
+    delete: async (id) => {
+        const [result] = await db.query(
+            `DELETE FROM tipo_servicio WHERE id_tipo_servicio = ?`, [id]
+        );
+        return result.affectedRows > 0;
+    }
+
+}; 
+
+exports = typeService;
