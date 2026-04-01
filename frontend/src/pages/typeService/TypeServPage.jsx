0b53d45a85ef4a7bdfd5c89e25d61cf4e09983ab
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-04-01 10:21:27 -0500

    Código TypeServPage

---
+++ frontend/src/pages/typeService/TypeServPage.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import TypeServTable from "../../components/typeService/TypeServTable";
+import ModalEditAgrTs from "../../components/typeService/ModalEditAgrTs";
+
+function TypeServPage() {
+  const [tipServ, setTipServ] = useState([]);
+  const [idSeleccionado, setIdSeleccionado] = useState(null);
+  const [busqueda, setBusqueda] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+   // Paginación
+  const [pagina, setPagina] = useState(1);
+  const tipServPorPagina = 5;
+
+  const getTipServ = () => {
+    axios.get("http://localhost:4000/api/tipo_servicio/listar")
+      .then(res => setTipServ(res.data))
+      .catch(err => console.error(err));
+  };
+
+  useEffect(() => {
+    getTipServ();
+  }, []);
+
+  // Filtrar usuarios según búsqueda
+  const tipServFiltrados = tipServ.filter(ts =>
+    (ts.nombre_servicio || "").toLowerCase().includes(busqueda.toLowerCase())
+  );
+
+  // Calcular usuarios de la página actual
+  const inicio = (pagina - 1) * tipServPorPagina;
+  const fin = inicio + tipServPorPagina;
+  const tipServPaginados = tipServFiltrados.slice(inicio, fin);
+
+  // Número total de páginas
+  const totalPaginas = Math.ceil(tipServFiltrados.length / tipServPorPagina);
+
+  return (
+    <div className="container mt-4">
+      <h2>Gestión de Tipo servcio</h2>
+
+      {/* Barra de acciones */}
+      <div className="d-flex justify-content-between mb-3">
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setIdSeleccionado(null);
+            setShowModal(true);
+          }}
+        >
+          Agregar tipo servicio
+        </button>
+
+        <input
+          type="text"
+          className="form-control w-50"
+          placeholder="Buscar tipo servicio"
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+      </div>
+
+      {/* Tabla */}
+      <TypeServTable
+        tipServ={tipServPaginados}
+        setIdSeleccionado={(u) => {
+          setIdSeleccionado(u);
+          setShowModal(true);
+        }}
+        getTipServ={getTipServ}
+      />
+
+      {/* Paginador */}
+      <div className="d-flex justify-content-center mt-3">
+        <nav>
+          <ul className="pagination">
+            <li className={`page-item ${pagina === 1 ? "disabled" : ""}`}>
+              <button className="page-link" onClick={() => setPagina(pagina - 1)}>
+                Anterior
+              </button>
+            </li>
+
+            {Array.from({ length: totalPaginas }, (_, i) => (
+              <li key={i} className={`page-item ${pagina === i + 1 ? "active" : ""}`}>
+                <button className="page-link" onClick={() => setPagina(i + 1)}>
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+
+            <li className={`page-item ${pagina === totalPaginas ? "disabled" : ""}`}>
+              <button className="page-link" onClick={() => setPagina(pagina + 1)}>
+                Siguiente
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* Modal de agregar/editar */}
+      {showModal && (
+        <ModalEditAgrTs
+          idSeleccionado={idSeleccionado}
+          getTipServ={getTipServ}
+          onClose={() => setShowModal(false)}
+          onSuccess={() => getTipServ()}
+        />
+      )}
+    </div>
+  );
+}
+
+export default TypeServPage;
