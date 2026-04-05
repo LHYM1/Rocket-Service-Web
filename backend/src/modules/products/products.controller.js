d61308c1a7e5e626c6df723b883cdae866cceb81
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-04-04 19:38:55 -0500

    Código products.controller

---
+++ backend/src/modules/products/products.controller.js
@@ -0,0 +1,70 @@
+// en la constante insumos se guarda los datos de endPoint agregar, eliminar, actualizar y listar de la tabla insumos
+const insumos = require('./products.model');
+
+exports.listarInsumo = async (req, res) => {
+
+    try {
+        const insm = await insumos.findAll();
+        res.json(insm);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error al listar Insumos' });
+    }
+};
+
+exports.obtenerInsumo = async (req, res) => {
+    try {
+        const insm = await insumos.findById(req.params.id);
+        if (!insm) return res.status(404).json({ message: "Insumo no encontrado" });
+        res.json(insm);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.crearInsumo = async (req, res) => {
+    try {
+        const { codigo_insumo, nombre_insumo, precio_base } = req.body; 
+
+        if (!codigo_insumo || !nombre_insumo || !precio_base) {
+            return res.status(400).json({ 
+                message: "Todos los campos son obligatorios" 
+            });
+        }
+        
+        const id = await usuarios.create(req.body);
+
+        res.status(201).json({ 
+            message: "Insumo creado correctamente",
+        }); 
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.actualizarInsumo = async (req, res) => {
+    try {
+        const actualizado = await insumos.update(req.params.id, req.body);
+
+        if (!actualizado) {
+            return res.status(404).json({ message: "Insumo no encontrado" });
+        }
+        res.json({ message: "Insumo actualizado correctamente" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+exports.eliminarInsumo = async (req, res) => {
+    try {
+        const eliminado = await insumos.delete(req.params.id);
+        if (!eliminado) 
+            return res.status(404).json({
+                message: "Insumo no encontrado" 
+            });
+        res.json({ message: "Insumo eliminado" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
