d5cc564b5ddfca947d073d77bbf92f4a927a887c
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-04-04 19:27:19 -0500

    Código products.model

---
+++ backend/src/modules/products/products.model.js
@@ -0,0 +1,82 @@
+const db = require('../../config/db');
+
+const product = {
+    findAll: async () => { // Listar
+        const [rows] = await db.query(
+            `SELECT 
+                p.id_insumo,
+                p.codigo_insumo,
+                ct.nombre AS categoria,
+                und.nombre AS unidad_de_medida,
+                p.nombre_insumo,
+                p.precio_base
+            FROM insumos p
+            LEFT JOIN categoria ct ON p.id_categoria = ct.id_categoria
+            LEFT JOIN unidad_de_medida und ON p.id_unidad_medida = und.id_unidad_medida
+        `);
+        return rows;
+    },
+
+    // consultar por id
+    findById : async (id) => {
+        const [rows] = await db.query(`SELECT * FROM insumos
+        WHERE id_insumo  = ?`, [id]);
+        return rows[0];
+    },
+
+    // crear
+    create: async (data) => {
+        const {
+            codigo_insumo,
+            nombre_insumo,
+            precio_base
+        } = data;
+
+        const [result] = await db.query(
+            `INSERT INTO insumos (codigo_insumo, nombre_insumo,
+            precio_base) VALUES (?, ?, ?)`,
+
+            [codigo_insumo, nombre_insumo, precio_base]
+        );
+        return result.insertId;
+    },
+
+    // actualizar
+    update : async (id, data) => {
+        const {
+            codigo_insumo,
+            id_categoria, 
+            id_unidad,
+            nombre_insumo,
+            precio_base
+        } = data;
+
+        const [result] = await db.query (
+            `UPDATE insumos SET codigo_insumo = ?,
+            id_categoria = ?, id_unidad = ?,
+            nombre_insumo = ?, precio_base 
+            WHERE id_tipo_servicio = ?`,
+
+            [
+                codigo_insumo,
+                id_categoria,
+                id_unidad,
+                nombre_insumo,
+                precio_base,
+                id
+            ]
+        );
+        return result.affectedRows > 0;
+    },
+
+    // eliminar
+    delete: async (id) => {
+        const [result] = await db.query(
+            `DELETE FROM insumos WHERE id_insumo  = ?`, [id]
+        );
+        return result.affectedRows > 0;
+    }
+
+}; 
+
+module.exports = product;
