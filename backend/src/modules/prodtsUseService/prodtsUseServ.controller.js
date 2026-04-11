e650fddbc2341b4790da4d4f5fff9853723957d2
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-04-10 20:08:28 -0500

    Código producto usado en servicio

---
+++ backend/src/modules/prodtsUseService/prodtsUseServ.controller.js
@@ -0,0 +1,82 @@
+import ProdtUseServ from './prodtsUseServ.model.js';
+
+export const listarInsUsedServ = async (req, res) => {
+    try {
+        const prdtUServ = await ProdtUseServ.findAll();
+        console.log("Datos obtenidos:", prdtUServ);
+        res.json(prdtUServ);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const obtenerInsUsedServ = async (req, res) => {
+    try {
+       const prdtUServ = await ProdtUseServ.findById(req.params.id);
+       if (!typeServ) return res.status(404).json({ message: "Insumos usado en servicio no encontrado"})
+       res.json(prdtUServ);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    } 
+}
+
+export const crearInsUsedServ = async (req, res) => {
+    try {
+        const {
+            id_orden , 
+            id_insumo, 
+            cantidad, precio_unitario
+        } = req.body;
+        
+        if (!id_orden || !id_insumo ||
+            !cantidad || !precio_unitario) {
+            return res.status(400).json({ 
+                message: "Todos los campos son obligatorios" 
+            });
+        }
+
+        const id = await ProdtUseServ.create(req.body);
+
+        res.status(201).json({ 
+            message: "Insumo usado en servicio creado exitosamente",
+        }); 
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const actInsUsedServ = async (req, res) => {
+    try {
+        const actualizado = await ProdtUseServ.update(req.params.id, req.body);
+
+        if (!actualizado) {
+            return res.status(404).json({ message: "Insumo usado en servicio no encontrado" });
+        }
+        res.json({ message: "Insumo usado en servicio actualizado correctamente" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const eliminarInsedServ = async (req, res) => {
+    try {
+        const eliminado = await ProdtUseServ.delete(req.params.id);
+        if (!eliminado) 
+            return res.status(404).json({
+                message: "Insumo usado en servicio no encontrado" 
+            });
+        res.json({ message: "Insumo usado en servicio eliminado" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// Exportación por defecto para que el import en las rutas funcione correctamente
+export default {
+    listarInsUsedServ,
+    obtenerInsUsedServ,
+    crearInsUsedServ,
+    actInsUsedServ,
+    eliminarInsedServ
+};
