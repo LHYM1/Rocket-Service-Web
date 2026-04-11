de3481bd2c403b6d35aa0a878afff78eeaff8b89
Juan-dev-25 <bernaltorresjuancamilo@gmail.com>
2026-04-11 08:51:47 -0500

    Actualizando consultas orders.model

--- backend/src/modules/orders/orders.model.js
+++ backend/src/modules/orders/orders.model.js
@@ -3,32 +3,7 @@ import db from '../../config/db.js';
 const OrdenServicio = {
     // Traer todas las órdenes con Nombres de Técnicos y Estados (JOIN)
     findAll: async () => {
-        const query = (`
-            SELECT o.id_orden,
-                o.codigo_orden,
-                m.placa,
-
-                CONCAT(c.nombre, '', c.apellido) AS cliente,
-                CONCAT(t.nombre, '', t.apellido) AS tecnico,
-
-                o.descripcion_del_problema,
-                e.nombre_estado,
-                ts.nombre_servicio,
-
-                o.fecha_de_creacion,
-                o.fecha_finalizacion_estimada
-            
-            FROM ordenes_de_servicio o
-
-            LEFT JOIN motocicleta m ON o.id_moto = m.id_moto
-            LEFT JOIN usuarios c ON o.id_usuario = c.id_usuario AND c.id_tipo_usuario = 1
-            LEFT JOIN usuarios t ON o.id_tecnico_asignado = t.id_usuario AND t.id_tipo_usuario = 2
-
-            LEFT JOIN tipo_servicio ts ON o.id_tipo_servicio = ts.id_tipo_servicio
-            LEFT JOIN estado_de_orden_de_servicio e ON o.id_estado_de_servicio = e.id_estado_de_servicio
-            ORDER BY o.fecha_de_creacion DESC
-
-        `);
+        const query = ('SELECT * FROM ordenes_de_servicio');
         const [rows] = await db.query(query);
         return rows;
     },
@@ -40,30 +15,63 @@ const OrdenServicio = {
 
     create: async (data) => {
         const { 
-            descripcion_del_problema, 
-            fecha_finalizacion_estimada
+            codigo_orden, 
+            id_moto,
+            id_usuario,
+            id_tecnico_asignado,
+            id_tipo_servicio,
+            id_estado_de_servicio,
+            fecha_finalizacion_estimada,
+            descripcion_del_problema
+
         } = data;
 
         const [result] = await db.query(
-            `INSERT INTO ordenes_de_servicio 
-            (descripcion_del_problema, fecha_finalizacion_estimada) 
-            VALUES (?, ?)`,
-            [descripcion_del_problema, fecha_finalizacion_estimada, ]
+            `INSERT INTO ordenes_de_servicio
+            
+            (codigo_orden, id_moto, id_usuario, id_tecnico_asignado, id_tipo_servicio, id_estado_de_servicio,
+            fecha_finalizacion_estimada, descripcion_del_problema)
+
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+            [
+                codigo_orden, id_moto, id_usuario, id_tecnico_asignado,
+                id_tipo_servicio, id_estado_de_servicio, fecha_finalizacion_estimada,
+                descripcion_del_problema
+            ]
         );
         return result.insertId;
     },
 
     update: async (id, data) => {
         const { 
-            descripcion_del_problema, 
-            fecha_finalizacion_estimada 
+            codigo_orden, 
+            id_moto,
+            id_usuario,
+            id_tecnico_asignado,
+            id_tipo_servicio,
+            id_estado_de_servicio,
+            fecha_finalizacion_estimada,
+            descripcion_del_problema 
         } = data;
 
         const [result] = await db.query(
             `UPDATE ordenes_de_servicio 
-             SET descripcion_del_problema = ?, fecha_finalizacion_estimada = ?, 
+             SET codigo_orden = ?, id_moto = ?, 
+             id_usuario = ?, id_tecnico_asignado = ?,
+             id_tipo_servicio = ?, id_estado_de_servicio = ?,
+             fecha_finalizacion_estimada = ?, descripcion_del_problema = ?
+
              WHERE id_orden = ?`,
-            [descripcion_del_problema, fecha_finalizacion_estimada, id]
+            [
+                codigo_orden, 
+                id_moto,
+                id_usuario,
+                id_tecnico_asignado,
+                id_tipo_servicio,
+                id_estado_de_servicio,
+                fecha_finalizacion_estimada,
+                descripcion_del_problema,  
+            id]
         );
         return result.affectedRows > 0;
     },
